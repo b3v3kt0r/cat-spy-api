1243d0db2c6465144f86c4195ec6f909095879aa
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-11-19 17:17:02 +0200

    add base models

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    years_of_experience = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
--- missions/models.py
+++ missions/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from cats.models import Cat
+
+
+class Mission(models.Model):
+    STATUS_CHOICES = (
+        ("PENDING", "Pending"),
+        ("IN_PROGRESS", "In Progress"),
+        ("COMPLETED", "Completed"),
+    )
+
+    name = models.CharField(max_length=100)
+    cats = models.ForeignKey(Cat, on_delete=models.SET_NULL, null=True, related_name="missions")
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="PENDING")
+    targets = models.ForeignKey("Target", on_delete=models.SET_NULL, null=True, related_name="missions")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Target(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    notes = models.TextField()
+    status = models.CharField(max_length=100)
