7607434e67bf9e24bc3b997dd227ef3d9d84ab6b
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-11-19 17:39:33 +0200

    add base views

--- missions/views.py
+++ missions/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from missions.models import Mission, Target
+from missions.serializers import MissionSerializer, TargetSerializer
+
+
+class MissionViewSet(viewsets.ModelViewSet):
+    queryset = Mission.objects.all()
+    serializer_class = MissionSerializer
+
+
+class TargetViewSet(viewsets.ModelViewSet):
+    queryset = Target.objects.all()
+    serializer_class = TargetSerializer
